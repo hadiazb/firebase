7a1dc4c69cfa523e42146861b2440142ff56afd6
hadiazb <hadiazb@unal.edu.co>
2020-11-02 23:08:33 -0500

    [ADD] auth with google

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from 'firebase';
+import './style.scss';
 
 const Home = () => {
+	const [user, setUser] = useState(null);
+
+	const handleAuth = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+
+		firebase
+			.auth()
+			.signInWithPopup(provider)
+			.then((result) => setUser(result.user))
+			.catch((error) =>
+				console.log(`Error ${error.code}: ${error.message}`)
+			);
+	};
+
+	const renderLoginButton = () => {
+		if (user !== null) {
+			return (
+				<div className='login__data'>
+					<img
+						className='login__data-img'
+						src={user.photoURL}
+						alt={user.displayName}
+					/>
+					<p className='login__data-message'>
+						Hola, {user.displayName}! has hecho login en nuestra
+						app
+					</p>
+				</div>
+			);
+		} else {
+			return (
+				<button className='login__button' onClick={handleAuth}>
+					Login con Google
+				</button>
+			);
+		}
+	};
+
 	return (
-		<div>
-			<h1>Hello</h1>
+		<div className='login'>
+			<h1 className='login__title'>Login</h1>
+			{renderLoginButton()}
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './router/App';
 import './styles/Global.scss';
+
+firebase.initializeApp({
+	apiKey: 'AIzaSyA1mByeK5flJ6Ur8wHRSKuzT5x5mofcuf0',
+	authDomain: 'filestorage-b0456.firebaseapp.com',
+	databaseURL: 'https://filestorage-b0456.firebaseio.com',
+	projectId: 'filestorage-b0456',
+	storageBucket: 'filestorage-b0456.appspot.com',
+	messagingSenderId: '341035177924',
+	appId: '1:341035177924:web:1a782d7eb7cbd25b67f691',
+	measurementId: 'G-QHHTYR9FCP',
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));
