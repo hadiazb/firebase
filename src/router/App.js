4cc45d48b8191eb0f7609bb0a89e7cb34b69fd1e
hadiazb <hadiazb@unal.edu.co>
2020-11-02 22:03:19 -0500

    [START] Project

---
+++ src/router/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+} from 'react-router-dom';
+import Home from '../containers/Home/index';
+
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route path='/' component={Home} exact />
+			</Switch>
+		</Router>
+	);
+};
+
+export default App;
